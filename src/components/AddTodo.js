4b63eebba582e61e38d55aba968ec7ff0dedff32
Ranjan11022 <ranjankr11022000@gmail.com>
2022-12-20 10:09:05 +0530

    React Assingment

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,9 +2,22 @@ import React from "react"
 
 const AddTodo = ({ dispatch }) => {
 
+    const handleSubmit = (e) =>{
+        console.log(e.target.title.value); 
+        e.preventDefault()
+        let todoData ={id: Date.now(),title: e.target.title.value}
+        dispatch({type:"ADD_TODO", payload: todoData})
+        e.target.reset()
+        
+    }
 
     return (
-        <></>
+        <div className="form-wrapper">
+            <form onSubmit={handleSubmit} id="todo-form">
+                <input name="title" id="todo-input"></input>
+                <button type="submit">Add</button>
+            </form>
+            </div>
     )
 }
 
